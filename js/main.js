44a144818301df2bb7720c97edbc54ed928073b7
User <vazgenaleksanyan9092@gmail.com>
2020-03-18 10:28:23 +0400

    правки по страницам

--- js/main.js
+++ js/main.js
@@ -15,8 +15,22 @@ function onToggleSelect(t) {
 }
 
 document.addEventListener('click', function (e) {
-    if(e.target.parentNode.className.match('custom-select__input')) return;
-    document.querySelectorAll('.custom-select__input.active').forEach(function (elem) {
-        elem.classList.remove('active');
-    })
+    if(e.target.parentNode.className.length){
+        if(e.target.parentNode.className.match('custom-select__input') || e.target.parentNode.className.match('autocomplete-select')) return;
+        document.querySelectorAll('.custom-select__input.active, .input.active').forEach(function (elem) {
+            elem.classList.remove('active');
+        })
+    }
 });
+
+function toggleTabs(id, t) {
+    document.querySelector('.admin_tab.active').classList.remove('active');
+    document.querySelector('.admin_content.active').classList.remove('active');
+    document.getElementById(id).classList.add('active');
+    t.classList.add('active');
+}
+
+function onAutocomplete(id, t) {
+    document.getElementById(id).value = t.innerHTML;
+    document.getElementById(id).classList.remove('active');
+}
